6e511ec75d4fad6d80597540be2f61d03a45ad60
manuvillalba-uclm <mvillalbamontalban@gmail.com>
2019-10-03 15:39:34 +0200

    Funciona el JSON, he usado otra libreía distinta que es de google también

---
+++ src/Cube.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Cube{
+    private int[][] BACK;
+    private int[][] DOWN;
+    private int[][] FRONT;
+    private int[][] LEFT;
+    private int[][] RIGHT;
+    private int[][] UP;
+/*
+    Cube(int[][] back, int[][] down, int[][] front, int[][] left, int[][] right, int[][] up){
+        this.back=back;
+        this.down=down;
+        this.front=front;
+        this.left=left;
+        this.right=right;
+        this.up=up;
+
+    }
+
+ */
+    public String toString() {
+        return " BACK= " + Arrays.deepToString(BACK) +
+                "\n DOWN= " + Arrays.deepToString(DOWN) +
+                "\n FRONT=" + Arrays.deepToString(FRONT) +
+                "\n LEFT= " + Arrays.deepToString(LEFT) +
+                "\n RIGHT=" + Arrays.deepToString(RIGHT) +
+                "\n UP=   " + Arrays.deepToString(UP);
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+import com.google.gson.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Gson gson = new Gson();
+
+        try (Reader reader = new FileReader("cubo1.json")) {
+            Cube cube = gson.fromJson(reader, Cube.class);
+
+            System.out.println(cube);
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+    }
+}
