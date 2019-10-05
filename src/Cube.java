f3cb0330e4920d69f8d79273f11110528dd112d1
pablo <pablotomas.toledano@alu.uclm.es>
2019-10-05 13:56:22 +0200

    Added moveB and completed the other moves

--- src/Cube.java
+++ src/Cube.java
@@ -44,6 +44,14 @@ public class Cube implements Cloneable {
             DOWN[i][n]=FRONT[i][n];
             FRONT[i][n]=UP[UP.length-i-1][UP.length-1-n];
             UP[UP.length-i-1][UP.length-1-n]=auxB[i][n];
+        }
+        switch (n){
+            case 0:
+                LEFT=rotateClockWise(LEFT);
+                break;
+            case 2:
+                RIGHT=rotateClockWise(RIGHT);
+                break;
 
         }
     }
@@ -54,10 +62,44 @@ public class Cube implements Cloneable {
            LEFT[i][LEFT.length-1-n]=FRONT[n][i];
            FRONT[n][i]=RIGHT[i][n];
            RIGHT[RIGHT.length-1-i][RIGHT.length-1-n]=auxB[BACK.length-n-1][BACK.length-1-i];
+        }
+        switch (n){
+            case 0:
+                DOWN=rotateClockWise(DOWN);
+                break;
+            case 2:
+                UP=rotateClockWise(UP);
+                break;
 
         }
     }
     public void moveB(int n){
+        int[][] auxL = Arrays.stream(LEFT).map(int[]::clone).toArray(int[][]::new);
+        for(int i=0;i<BACK.length;i++){
+            LEFT[n][i]=UP[n][i];
+            UP[n][i]=RIGHT[n][i];
+            RIGHT[n][i]=DOWN[n][i];
+            DOWN[n][i]=auxL[n][i];
+            //falta mover back y front
+        }
+        switch (n){
+            case 0:
+                BACK=rotateClockWise(BACK);
+                break;
+            case 2:
+                FRONT=rotateClockWise(FRONT);
+                break;
 
+        }
+    }
+    private int[][] rotateClockWise(int[][] array) {
+        int size = array.length;
+        int[][] aux = new int[size][size];
+
+        for (int i = 0; i < size; ++i)
+            for (int j = 0; j < size; ++j)
+                aux[i][j] = array[size - j - 1][i];
+
+        return aux;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
             Cube cube = gson.fromJson(reader, Cube.class);
             System.out.println(cube);
             System.out.println("--------------------------------");
-            cube.moveD(0);
+            cube.moveL(2);
             System.out.println(cube);
         } catch (IOException e) {
             e.printStackTrace();
