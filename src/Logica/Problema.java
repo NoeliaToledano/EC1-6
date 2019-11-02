403f2e687b8a0bd88e0ee6557c78cb903f2b9579
NoeliaToledano <noeliatoledanocampos@hotmail.com>
2019-11-02 17:13:18 +0100

    CrearSolucion

--- src/Logica/Problema.java
+++ src/Logica/Problema.java
@@ -72,10 +72,10 @@ public class Problema {
     public void CrearSolucion(NodoArbol nodoObjetivo){
 
         System.out.println("["+nodoObjetivo.getId()+"](["+nodoObjetivo.getAccion()+"]"+nodoObjetivo.getEstado().getCubo().toMD5()+",c="+nodoObjetivo.getCosto_camino()+",p="+nodoObjetivo.getD()+",f="+nodoObjetivo.getF()+")");
-        NodoArbol nodo=nodoObjetivo.getPadre();
-        if (!nodoObjetivo.EsPadre()) {
 
-            while (!nodoObjetivo.EsPadre()) {
+        if (!nodoObjetivo.EsPadre()) {
+            NodoArbol nodo=nodoObjetivo.getPadre();
+            while (!nodo.EsPadre()) {
                 System.out.println("[" + nodo.getId() + "]([" + nodo.getAccion() + "]" + nodo.getEstado().getCubo().toMD5() + ",c=" + nodo.getCosto_camino() + ",p=" + nodo.getD() + ",f=" + nodo.getF() + ")");
                 nodo = nodo.getPadre();
             }
